5ce59c2ed7743e63ab35193a462a2acaff4a105b
Anshuman <singh.anshuman2600@gmail.com>
2024-07-08 02:04:18 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js"
 import messageRoutes from "./routes/message.routes.js"
+import cookieParser from "cookie-parser"
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import userRoutes from "./routes/user.routes.js";
 
-import connectToMongoDB from "./DB/mongoDB.js";
 const app=express();
 const PORT=process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json());
+app.use(cookieParser());
 app.use("/",authRoutes)
 app.use("/api/messages",messageRoutes)
+app.use("/api/users",userRoutes)
+
 // app.get('/',(req,res)=>{
 //     res.send("helo world");
 // })
